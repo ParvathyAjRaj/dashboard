6fcbf8365d6ba7b990fffdf797709f850aecaff0
ParvathyAjRaj <paruvathy99@gmail.com>
2024-04-17 00:37:52 +0530

    added comments and notifications

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,29 +1,47 @@
 import React from "react";
-import {Typography,Image,Space, Badge} from "antd";
+import {Typography,Image,Space, Badge, Drawer, List} from "antd";
 import {MailOutlined, BellFilled} from '@ant-design/icons';
 import './Header.css';
+import {getComments, getOrders} from "../API/api";
+import { useState , useEffect } from "react";
 
 function Header(){
-return(
-    <div className="header">
-        <Image
-            width={100}
-            className="profile"
-            alt="sample"
-            src="motivation_img.jpg"
-        />
-        <Typography.Title style={{paddingBottom:5,marginRight:50}}>Cart Dashboard</Typography.Title>
-        <Space>
-            <Badge count={10} dot>
-                <MailOutlined style={{fontSize:24 , paddingRight:5}}></MailOutlined>
-            </Badge>
-            <Badge count={20}>
-                <BellFilled style={{fontSize:24}}></BellFilled>
-            </Badge>
-        </Space>
-        
-    </div>
-);
+
+    const [comments,setComments] = useState([]);
+    const [orders,setOrders] = useState([]);
+    const [commentsOpen,setCommentsOpen] = useState(false);
+    const [notificationsOpen,setNotificationsOpen] = useState(false);
+
+    useEffect(()=>{
+        getComments().then(res=>{setComments(res.comments)});
+        getOrders().then(res=>{setOrders(res.products)});
+    },[])
+
+    return(
+        <div className="header">
+            <Image
+                width={100}
+                className="profile"
+                alt="sample"
+                src="motivation_img.jpg"
+            />
+            <Typography.Title style={{paddingBottom:5,marginRight:50}}>Cart Dashboard</Typography.Title>
+            <Space>
+                <Badge count={comments.length} dot>
+                    <MailOutlined style={{fontSize:24 , paddingRight:5}} onClick={()=>{setCommentsOpen(true)}}></MailOutlined>
+                </Badge>
+                <Badge count={orders.length}>
+                    <BellFilled style={{fontSize:24}} onClick={()=>{setNotificationsOpen(true)}}></BellFilled>
+                </Badge>
+            </Space>
+            <Drawer title="Comments" open={commentsOpen} onClose={()=>{setCommentsOpen(false)}}>
+                <List dataSource={comments} renderItem={(item) => {return(<List.Item>`<Typography.Text strong>{item.body}</Typography.Text> - {item.user.username}`</List.Item>)}}></List>
+            </Drawer>
+            <Drawer title="Notifications" open={notificationsOpen} onClose={()=>{setNotificationsOpen(false)}}>
+                <List dataSource={orders} renderItem={(item) => {return(<List.Item><Typography.Text strong>{item.title}</Typography.Text>  has been ordered</List.Item>)}}></List>
+            </Drawer>
+        </div>
+    );
 }
 
 export default Header;
